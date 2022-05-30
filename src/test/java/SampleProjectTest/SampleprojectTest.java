b4fc80bdceeace4b5f7cafd1163750d6b54a9408
sin42 <sin42@DESKTOP-MQ9KKGC>
2022-05-30 17:47:11 +0530

    NewProject

---
+++ src/test/java/SampleProjectTest/SampleprojectTest.java
@@ -0,0 +1,17 @@
+package SampleProjectTest;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SampleprojectTest {
+	
+		@Test
+		public void testName() throws Exception 
+		{
+			Reporter.log("Heyyy",true);
+			Reporter.log("Heyy-Heyyy",true);
+			Reporter.log("Heyyy-Heyyy-Heyyy",true);
+			Reporter.log("Byyeee",true);
+		}
+	}
+
