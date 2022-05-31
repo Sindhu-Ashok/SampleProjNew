abe1c94a1e51ad1412728e7f378811d4ac79f793
Sindhu-Ashok <106369259+Sindhu-Ashok@users.noreply.github.com>
2022-05-31 09:54:06 +0530

    Updated

--- src/test/java/SampleProjectTest/SampleprojectTest.java
+++ src/test/java/SampleProjectTest/SampleprojectTest.java
@@ -8,10 +8,12 @@ public class SampleprojectTest {
 		@Test
 		public void testName() throws Exception 
 		{
+			String browser = System.getProperty("BROWSER");
 			Reporter.log("Heyyy",true);
 			Reporter.log("Heyy-Heyyy",true);
 			Reporter.log("Heyyy-Heyyy-Heyyy",true);
 			Reporter.log("Byyeee",true);
+			Reporter.log(browser,true);
 		}
 	}
 
